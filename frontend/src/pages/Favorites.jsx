23060b90839e8c49c0245b18c3037da2b1e2f745
Eric Van Hezewijk <ericvanhezewijk@uvic.ca>
2025-01-28 00:20:25 -0800

    rendering of favorite movie list is now working and persists between pages

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import '../css/Favorites.css'
+import { useMovieContext } from '../context/MovieContext'
+import MovieCard from '../components/MovieCard'
 
 function Favorites() {
+
+  const {favorites} = useMovieContext()
+  console.log(favorites)
+
   return (
-    <div className='favorites-empty'>
-      <h2>No Favorite Movies Yet</h2>
-      <p>Start adding movies to your favorties and they will appear here.</p>
-    </div>
+    <>
+      {favorites.length == 0 ? (<div className='favorites-empty'>
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorties and they will appear here.</p>
+      </div>
+      ) : (
+        <div className="movies-grid">
+          {favorites.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
+
+    </>
   )
 }
 
